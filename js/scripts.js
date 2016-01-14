02236edf490e6d42c3c3b6afdfa7c08dfb483db2
Alex Larson and Erik Tolentino <tolentino3575@gmail.com>
2016-01-14 15:12:49 -0800

    added Loops

--- js/scripts.js
+++ js/scripts.js
@@ -24,5 +24,78 @@ $("#hover").hover(function () {
   $("#hover").toggleClass("bigimg");
 })
 
+$("#button").click(function(event) {
+  var age = parseInt($("#age").val());
+console.log(age);
+
+  if (age >= 22) {
+    $("#text").text("You're old.")
+  } else if (age < 20) {
+    $("#text").text("You're not old.")
+  } else if (age === 21) {
+    $("#text").text("You're 21")
+  }else {
+    $("#text").text("Enter Something")
+  }
+event.preventDefault()
+})
+
+$("button#on").click(function(){
+  $("body").removeClass();
+  $("body").addClass("blue-background")
+})
+
+$("button#off").click(function(){
+  $("body").removeClass();
+})
+
+$("#arrayClick").click(function () {
+  var numArray = [1,2,3,4,5,6]
+  numArray.forEach(function (number) {
+  $("#arrayPrint").append(", " + number);
+  })
+})
+
+$("#arrayAdd").click(function() {
+  var total = 0
+  var num = [1, 2, 3, 4, 5]
+  num.forEach(function(num) {
+    total += num;
+    $("#arrayAdd").text(total);
+    $("#arrayAdd").after(" " +total);
+  })
+})
+
+$("#arrayMultiply").click(function() {
+  var total = 1
+  var num = [1, 2, 3, 4, 5]
+  num.forEach(function(num) {
+    total *= num;
+    $("#arrayMultiply").text(total);
+    $("#arrayMultiply").after(" " +total);
+
+  })
+})
+
+$("#clickFor").click(function() {
+var num = $("#inputFor").val()
+for(var i=0; i<=num; i+=1) {
+$("#printFor").append("<li>"+i+"</li>")
+}
+})
+
+$("#click").click(function() {
+var array = [1, 2, 3, 4, 5]
+for (var i=0; i<=array.length; i++ )
+$("#click").append(i)
+
+
+})
+
+
+
+
+
+
 
 })
